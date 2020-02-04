060193dfb65941f17484be2f1cd66e4bfb725c10
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-02-04 21:20:36 +0900

    [ADD] print output text in ViewController

--- YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
+++ YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        APIClient().convert("林大地") { text in
+            print(text)
+        }
     }
 }
 
