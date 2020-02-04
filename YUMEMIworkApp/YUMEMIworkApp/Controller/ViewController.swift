65e73913cff8b9fa74137757f98186b7b9388c9f
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-02-04 20:26:45 +0900

    [ADD] main view controller

--- YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
+++ YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let mainViewController = MainViewController()
+        self.present(mainViewController, animated: false, completion: nil)
     }
 
 
---
+++ YUMEMIworkApp/YUMEMIworkApp/View/MainView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  YUMEMIworkApp
+//
+//  Created by Daichi Hayashi on 2020/02/04.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+}
