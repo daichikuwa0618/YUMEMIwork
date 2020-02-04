24f2f9fa1d520704d2ecbb62f2009283c0895437
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-02-05 01:34:52 +0900

    [ADD] very basic view layouts, features (input, button, output)

--- YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
+++ YUMEMIworkApp/YUMEMIworkApp/Controller/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import UITextView_Placeholder
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        APIClient().convert("林大地") { text in
-            print(text)
+        inputTextView.placeholder = "変換したいテキストを入力してください"
+        }
+    }
+    @IBAction func tapConvert(_ sender: Any) {
+        let japaneseSentence: String = inputTextView.text ?? ""
+        APIClient().convert(japaneseSentence) { text in
+            self.outputLabel.text = text
         }
     }
 }
