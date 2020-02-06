5ab63e0f82f2d54096231304f2277118e313900e
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-02-06 16:14:05 +0900

    [ADD] new class file which set gradient color in storyboard

---
+++ YUMEMIworkApp/YUMEMIworkApp/View/GradientButton.swift
@@ -0,0 +1,21 @@
+//
+//  GradientButton.swift
+//  YUMEMIworkApp
+//
+//  Created by Daichi Hayashi on 2020/02/06.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class GradientButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
